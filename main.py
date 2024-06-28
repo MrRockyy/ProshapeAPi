ac91cec52f1d5732830226f1479f92fb400de54f
se;or de nos <you@example.com>
2024-06-28 09:47:03 -0500

    fdst

--- main.py
+++ main.py
@@ -213,4 +213,4 @@ def main():
         return jsonify({"msg": "User not found"}), 404
 
 if __name__ == '__main__':
-    app.run(port=os.getenv("PORT", default=5000), port=5000, debug=True)
+      app.run(debug=True, port=os.getenv("PORT", default=5000))
